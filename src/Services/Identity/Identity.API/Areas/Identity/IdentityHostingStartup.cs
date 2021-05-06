0915b25c5fe912097d9fdd8850bb80f722fb4398
zekeriya <zekeriyakocairi@gmail.com>
2021-05-06 09:36:09 +0300

    Add IdentityScaffolding

---
+++ src/Services/Identity/Identity.API/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Identity.API.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Identity.API.Areas.Identity.IdentityHostingStartup))]
+namespace Identity.API.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityAPIContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityAPIContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityAPIContext>();
+            });
+        }
+    }
+}
