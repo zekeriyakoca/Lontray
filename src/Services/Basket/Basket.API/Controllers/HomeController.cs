263ee41668115fd58ab9a1616baa6ef60bb937d8
zekeriya <zekeriyakocairi@gmail.com>
2021-05-15 13:27:39 +0300

    Initilaize Basket.API

---
+++ src/Services/Basket/Basket.API/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
