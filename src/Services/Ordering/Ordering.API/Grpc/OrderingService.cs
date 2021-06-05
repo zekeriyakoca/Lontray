e120903f808742f828b9cdaf7098493aa87d1e2c
zekeriya <zekeriyakocairi@gmail.com>
2021-06-05 22:23:16 +0300

    Add GRPC OrderingService server to Ordering MS

---
+++ src/Services/Ordering/Ordering.API/Grpc/OrderingService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Grpc
+{
+    public class OrderingService : Ordering.OrderingBase
+    {
+        private readonly ILogger<OrderingService> logger;
+
+        public OrderingService(ILogger<OrderingService> logger)
+        {
+            this.logger = logger;
+        }
+    }
+}
